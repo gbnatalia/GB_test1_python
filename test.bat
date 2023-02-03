b5bbd7e8549a9c163d0ae2381515078807e88f5f
GBNatalia <ngmarkiza@yandex.ru>
2023-02-03 20:48:15 +0300

    Добавление в проект файла тестирования проекта

---
+++ test.bat
@@ -0,0 +1,55 @@
+
+python notes.py --help
+python notes.py --HELP
+python notes.py --h
+
+python notes.py --print
+
+python notes.py --clear
+python notes.py --print
+
+python notes.py --add "Note 0" "Text Note 0"
+python notes.py --ADD "Note 1" "Text Note 1"
+python notes.py --a "Note 2" "Text Note 2"
+
+python notes.py --del "Note 0"
+python notes.py --list ALL
+python notes.py --DEL "Note 1"
+python notes.py --LIST all
+python notes.py --d "Note 2"
+python notes.py --list ALL
+python notes.py --d "Note 4"
+python notes.py --l ALL
+
+python notes.py --add "Note 0" "Text Note 0"
+python notes.py --ADD "Note 1" "Text Note 1"
+python notes.py --a "Note 2" "Text Note 2"
+
+python notes.py --e "Note 1"
+python notes.py --VIEW "Note 1"
+
+python notes.py --e "Note 2"
+python notes.py --v "Note 2"
+
+python notes.py --view "Note 3"
+
+python notes.py --rename "Note 2" "Note 3"
+python notes.py --list all
+
+python notes.py --rename "Note 3" "Note 2"
+python notes.py --LIST ALL
+
+python notes.py --rename "Note 2" "Note 4"
+python notes.py --l all
+
+python notes.py --e "Note 1"
+python notes.py --l all
+
+python notes.py --print
+
+python notes.py --list 2023-02-02
+python notes.py --list 2023-02-03
+
+
+
+
